b914b21df10fe7dc2996829eac29912aad5d083b
mikel evins <mevins@me.com>
2020-09-06 10:52:23 -0500

    added finalizeDelectus() function

--- testlecter.lisp
+++ testlecter.lisp
@@ -6,6 +6,13 @@
   :result-type :int
   :language :ansi-c)
 
+(fli:define-foreign-function 
+    (finalize-delectus "finalizeDelectus" :source)
+    ()
+  :result-type :int
+  :language :ansi-c)
+
 (fli:register-module "/Users/mikel/Workshop/src/lecter/libDelectus.dylib")
 
 (init-delectus)
+(finalize-delectus)
