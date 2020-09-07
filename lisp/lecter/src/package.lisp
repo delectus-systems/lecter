b5ad9d99cec124a7ac1aa36e7f1bd8111f1b64e5
mikel evins <mevins@me.com>
2020-09-07 17:04:39 -0500

    adding cffi interface to libDelectus

--- lisp/lecter/src/package.lisp
+++ lisp/lecter/src/package.lisp
@@ -1,4 +1,4 @@
 ;;;; package.lisp
 
 (defpackage #:lecter
-  (:use #:cl))
+  (:use #:cl #:cffi))
--- lisp/lecter/src/sbcl.lisp
+++ lisp/lecter/src/sbcl.lisp
@@ -6,3 +6,39 @@
 ;;; ---------------------------------------------------------------------
 
 (defparameter $libdelectus-path (asdf:system-relative-pathname :lecter "../../libDelectus.dylib"))
+
+(define-foreign-library libdelectus
+    (:darwin "libDelectus.dylib")
+    (t (:default "libDelectus.dylib")))
+   
+(defun load-libDelectus ()
+  (let ((cffi:*foreign-library-directories*
+         (cons $libdelectus-path cffi:*foreign-library-directories*)))
+    (use-foreign-library libdelectus)))
+
+;;; (load-libDelectus)
+
+;;; ---------------------------------------------------------------------
+;;; Delectus library api functions
+;;; ---------------------------------------------------------------------
+
+;;; gambit APIs
+
+(defcfun ("___release_string" %release-string) :void (s :string))
+
+;;; Delectus APIs
+
+(defcfun ("initDelectus" %init-delectus) :int)
+
+;;; (%init-delectus)
+
+(defcfun ("finalizeDelectus" %finalize-delectus) :int)
+
+(defcfun ("version" %delectus-version) :string)
+
+;;; (%delectus-version)
+
+(defcfun ("read_delectus_file" %read-delectus-file) :int (path :string))
+
+;;; (setf $movies-path "/Users/mikel/Workshop/src/delectus/test-data/Movies.delectus")
+;;; (with-foreign-string (s $movies-path)(%read-delectus-file s))
